390b1376eee052d0487d0a6baa14ac2424950c5c
Redul <65187040+Redul@users.noreply.github.com>
2020-07-17 00:29:25 +0300

    Update hello.py

--- web/hello.py
+++ web/hello.py
@@ -1,5 +1,5 @@
 def app(env, start_response):
   start_response('200 OK', [('Content-Type', 'text/plain')])
-  body = [bytes('\n'.join(environ['QUERY_STRING'].split('&')),
+  body = [bytes('\n'.join(env['QUERY_STRING'].split('&')),
                   encoding="ascii")]
   return body
