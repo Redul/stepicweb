e98af63531514d93d19fc009f759a0415079b8e1
Redul <65187040+Redul@users.noreply.github.com>
2020-07-16 22:54:08 +0300

    Create hello.py

---
+++ web/hello.py
@@ -0,0 +1,6 @@
+def app(env, start_response) {
+  start_response('200 OK', [('Content-Type', 'text/plain')])
+  body = [bytes('\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="ascii")]
+  return body
+}
