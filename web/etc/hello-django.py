f2f8e5270c608b63bfcdee4f322ebed1eb962d4c
olegsiy <senick.a@yandex.ru>
2020-07-25 21:54:01 +0300

    Initial commit.

--- web/etc/hello-django.py
+++ web/etc/hello-django.py
@@ -1,12 +1,12 @@
 CONFIG = {
   'mode': 'wsgi',
-  'working_dir': '/home/box/web',
-  'pythonpath': '/home/box/web',
+  'working_dir': '/home/box/web/ask',
+  'pythonpath': '/home/box/web/ask',
   'python': '/usr/bin/python3',
   'args': (
     '--bind=0.0.0.0:8080',
     '--workers=2',
     '--timeout=15',
-    'hello:application'
+    'ask.wsgi:application'
   )
 }
--- web/hello.py
+++ web/hello.py
@@ -1,4 +1,4 @@
-def app(environ, start_response):
+def application(environ, start_response):
   start_response('200 OK', [('Content-Type', 'text/plain')])
   body = [bytes('\n'.join(environ['QUERY_STRING'].split('&')),
                   encoding="ascii")]
